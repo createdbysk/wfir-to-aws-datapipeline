af2dfba541048cc01bcde99e0c669667e4187b01
Satish Kumar <kumarnotifications@gmail.com>
2018-10-15 19:20:27 -0500

    sql_script translator.

--- src/aws/datapipeline/task_translators.py
+++ src/aws/datapipeline/task_translators.py
@@ -1,2 +1,59 @@
-def translate_sql_script(task_ir, context):
-    raise NotImplementedError()
+import abc
+
+
+class __Translator(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __call__(self, task_ir, context):
+        template = self._get_template()
+        translated = self._translate(task_ir, context, template)
+        return translated
+
+    @abc.abstractmethod
+    def _get_template(self):
+        """
+        Implement this method in implementation classes to get the template for a given task type.
+        :returns The template
+        """
+
+    # noinspection PyMethodMayBeStatic
+    def _create_parameters(self, task_ir, context):
+        parameters = {
+            "context": context,
+            "task_ir": task_ir
+        }
+        return parameters
+
+    def _translate(self, task_ir, context, template):
+        import pystache
+        import json
+        parameters = self._create_parameters(task_ir, context)
+        print("Template", template)
+        translated_json = pystache.render(template, parameters)
+        print("Translated", translated_json)
+        translated = json.loads(translated_json)
+        return translated
+
+
+class _SqlScriptTranslator(__Translator):
+    def _create_parameters(self, task_ir, context):
+        import os
+        parameters = super(_SqlScriptTranslator, self)._create_parameters(task_ir, context)
+        file_path = task_ir["file_path"]
+        file_basename = os.path.basename(file_path)
+        task_suffix = os.path.splitext(file_basename)[0]
+        parameters["task_suffix"] = task_suffix
+        parameters["task_index"] = "{:03d}".format(context["task_index"])
+        return parameters
+
+    def _get_template(self):
+        return """
+{
+    "type": "SqlActivity",
+    "id": "{{ task_index }}_{{ task_suffix }}"
+}    
+"""
+
+
+# Module level symbols
+translate_sql_script = _SqlScriptTranslator()
--- test/aws/datapipeline/test_task_translators.py
+++ test/aws/datapipeline/test_task_translators.py
@@ -9,8 +9,7 @@ def compute_repr():
 @pytest.fixture()
 def context(compute_repr):
     yield {
-        "task_index": 1,
-        "compute": compute_repr
+        "task_index": 1
     }
 
 
@@ -18,23 +17,26 @@ def test_sql_script(context):
     from aws.datapipeline.task_translators import translate_sql_script
     # GIVEN
     file_path = "/path/to/sql_script.sql"
-    database = '"database": "database"'
+    database = "database"
+    compute = "compute"
 
     ir = {
         "type": "sql_script",
         "file_path": file_path,
-        "database": database
+        "database": database,
+        "compute": compute
     }
 
-    sql_script_definition = """
-{
-    "type": "SqlActivity",
-    "id": "001_sql_script",
-    "scriptUri": 
-    {database},
-    {compute}
-}
-""".format(database=database, compute=compute)
+    sql_script_definition = {
+        "type": "SqlActivity",
+        "id": "001_sql_script",
+        "scriptUri": "s3://wfir-bucket/project-path/path/to/sql_script.sql",
+        "database": {
+            "ref": database
+        },
+        "workerGroup": "worker-group"
+    }
+
     expected_result = sql_script_definition
 
     # WHEN
