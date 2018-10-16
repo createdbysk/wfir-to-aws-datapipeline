93148d17691900c249cf3d3496647bf81724057c
Satish Kumar <kumarnotifications@gmail.com>
2018-10-15 20:16:48 -0500

    sql_script translator.

--- src/aws/datapipeline/task_translators.py
+++ src/aws/datapipeline/task_translators.py
@@ -4,9 +4,9 @@ import abc
 class __Translator(object):
     __metaclass__ = abc.ABCMeta
 
-    def __call__(self, task_ir, context):
+    def __call__(self, task_ir, context_factory):
         template = self._get_template()
-        translated = self._translate(task_ir, context, template)
+        translated = self._translate(task_ir, context_factory, template)
         return translated
 
     @abc.abstractmethod
@@ -16,41 +16,29 @@ class __Translator(object):
         :returns The template
         """
 
-    # noinspection PyMethodMayBeStatic
-    def _create_parameters(self, task_ir, context):
-        parameters = {
-            "context": context,
-            "task_ir": task_ir
-        }
-        return parameters
+    # noinspection PyMethodMayBeStatic,PyUnusedLocal
+    def _create_parameters(self, task_ir, context_factory):
+        return {}
 
-    def _translate(self, task_ir, context, template):
+    def _translate(self, task_ir, context_factory, template):
         import pystache
         import json
+        renderer = pystache.Renderer(missing_tags="strict")
+        context = context_factory(renderer)
         parameters = self._create_parameters(task_ir, context)
-        print("Template", template)
-        translated_json = pystache.render(template, parameters)
+        translated_json = renderer.render(template, context, task_ir, parameters)
         print("Translated", translated_json)
         translated = json.loads(translated_json)
         return translated
 
 
 class _SqlScriptTranslator(__Translator):
-    def _create_parameters(self, task_ir, context):
-        import os
-        parameters = super(_SqlScriptTranslator, self)._create_parameters(task_ir, context)
-        file_path = task_ir["file_path"]
-        file_basename = os.path.basename(file_path)
-        task_suffix = os.path.splitext(file_basename)[0]
-        parameters["task_suffix"] = task_suffix
-        parameters["task_index"] = "{:03d}".format(context["task_index"])
-        return parameters
-
     def _get_template(self):
         return """
 {
     "type": "SqlActivity",
-    "id": "{{ task_index }}_{{ task_suffix }}"
+    "id": "{{ task_index }}_{{#filename_without_extension}}{{ file_path }}{{/filename_without_extension}}",
+    "scriptUri": "{{#deployed_path}}{{ file_path }}{{/deployed_path}}"
 }    
 """
 
--- test/aws/datapipeline/test_task_translators.py
+++ test/aws/datapipeline/test_task_translators.py
@@ -1,24 +1,55 @@
 import pytest
-
-
-@pytest.fixture()
-def compute_repr():
-    yield '"compute_key": "compute_value"'
-
+import os
 
 @pytest.fixture()
-def context(compute_repr):
-    yield {
-        "task_index": 1
-    }
+def deployed_path():
+    def impl(file_path):
+        file_basename = os.path.basename(file_path)
+        deployed_path = "s3://wfir-bucket/project-path/path/to/{filename}".format(filename=file_basename)
+        return deployed_path
+
+    yield impl
 
 
-def test_sql_script(context):
+# noinspection PyShadowingNames
+@pytest.fixture()
+def context_factory(deployed_path):
+    class Context(object):
+        def __init__(self, renderer):
+            self.__renderer = renderer
+
+        def filename_without_extension(self):
+            def impl(file_path_template):
+                file_path = self.__renderer.render(file_path_template, self.__renderer.context)
+                file_basename = os.path.basename(file_path)
+                without_extension = os.path.splitext(file_basename)[0]
+                return without_extension
+            return impl
+
+        def deployed_path(self):
+            def impl(file_path_template):
+                file_path = self.__renderer.render(file_path_template, self.__renderer.context)
+                return deployed_path(file_path)
+
+            return impl
+
+        def task_index(self):
+            return "001"
+
+    def factory(renderer):
+        return Context(renderer)
+
+    yield factory
+
+
+def test_sql_script(context_factory,
+                    deployed_path):
     from aws.datapipeline.task_translators import translate_sql_script
     # GIVEN
     file_path = "/path/to/sql_script.sql"
     database = "database"
     compute = "compute"
+    deployed_path = deployed_path(file_path)
 
     ir = {
         "type": "sql_script",
@@ -30,7 +61,7 @@ def test_sql_script(context):
     sql_script_definition = {
         "type": "SqlActivity",
         "id": "001_sql_script",
-        "scriptUri": "s3://wfir-bucket/project-path/path/to/sql_script.sql",
+        "scriptUri": deployed_path,
         "database": {
             "ref": database
         },
@@ -40,7 +71,7 @@ def test_sql_script(context):
     expected_result = sql_script_definition
 
     # WHEN
-    actual_result = translate_sql_script(ir, context)
+    actual_result = translate_sql_script(ir, context_factory)
 
     # THEN
     assert expected_result == actual_result
