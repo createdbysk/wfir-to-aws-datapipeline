73648c2245847c9a40df5eec5a77aec5748245e2
Satish Kumar <kumarnotifications@gmail.com>
2018-10-15 21:17:05 -0500

    sql_script translator.

--- src/aws/datapipeline/task_translators.py
+++ src/aws/datapipeline/task_translators.py
@@ -1,47 +1,42 @@
 import abc
+import os
 
 
-class __Translator(object):
+class _Translator(object):
     __metaclass__ = abc.ABCMeta
+    __TEMPLATES_DIRECTORY = "task_translator_templates"
+
+    def __init__(self, task_type):
+        self.__task_type = task_type
 
     def __call__(self, task_ir, context_factory):
-        template = self._get_template()
-        translated = self._translate(task_ir, context_factory, template)
+        template_path = self._get_template_path()
+        translated = self._translate(task_ir, context_factory, template_path)
         return translated
 
-    @abc.abstractmethod
-    def _get_template(self):
-        """
-        Implement this method in implementation classes to get the template for a given task type.
-        :returns The template
-        """
+    def _get_template_path(self):
+        directory = os.path.dirname(__file__)
+        absolute_path = os.path.abspath(directory)
+        templates_directory = os.path.join(absolute_path, _Translator.__TEMPLATES_DIRECTORY)
+        mustache_filename = "{task_type}.mustache".format(task_type=self.__task_type)
+        template_path = os.path.join(templates_directory, mustache_filename)
+        return template_path
 
     # noinspection PyMethodMayBeStatic,PyUnusedLocal
     def _create_parameters(self, task_ir, context_factory):
         return {}
 
-    def _translate(self, task_ir, context_factory, template):
+    def _translate(self, task_ir, context_factory, template_path):
         import pystache
         import json
         renderer = pystache.Renderer(missing_tags="strict")
         context = context_factory(renderer)
         parameters = self._create_parameters(task_ir, context)
-        translated_json = renderer.render(template, context, task_ir, parameters)
+        translated_json = renderer.render_path(template_path, context, task_ir, parameters)
         translated = json.loads(translated_json)
         context.add_standard_fields(translated)
         return translated
 
 
-class _SqlScriptTranslator(__Translator):
-    def _get_template(self):
-        return """
-{
-    "type": "SqlActivity",
-    "id": "{{ task_index }}_{{#filename_without_extension}}{{ file_path }}{{/filename_without_extension}}",
-    "scriptUri": "{{#deployed_path}}{{ file_path }}{{/deployed_path}}"
-}    
-"""
-
-
 # Module level symbols
-translate_sql_script = _SqlScriptTranslator()
+translate_sql_script = _Translator("sql_script")
